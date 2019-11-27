b119e2b62dfb88accb3b48506be63b1f56a757f0
Fye-valentine <55819209+Fye-valentine@users.noreply.github.com>
2019-11-27 16:20:07 +0800

    Create 237.删除链表中的节点.cpp

---
+++ java and cpp/linklist/237.删除链表中的节点.cpp
@@ -0,0 +1,15 @@
+/*请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+
+现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+    }
+};
